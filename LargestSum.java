037ce2da40b4e6003965bcaad7c577ead95dc168
akshaykolhe7 <akshaykolhe9923@gmail.com>
2024-12-20 21:26:52 +0530

    second commit

--- LargestSum.java
+++ LargestSum.java
@@ -10,8 +10,11 @@ public class LargestSum {
         int num3 = scanner.nextInt();
 
         int largest = Math.max(num1, Math.max(num2, num3));
+        double average = (num1 + num2 + num3) / 3.0;
+
         System.out.println("The largest number is: " + largest);
         System.out.println("The sum of the largest number is: " + largest);
+        System.out.println("The average of the three numbers is: " + average);
 
         scanner.close();
     }
